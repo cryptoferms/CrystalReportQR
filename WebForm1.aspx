34532707de3961f491187ea78296bba683c86cfb
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-27 13:26:01 -0300

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CrystalReportQR.WebForm1" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+        <div>
+            <h1>Crystal Report relátório teste QR Code</h1>
+        </div>
+        <br />
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+         <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using CrystalDecisions.Shared;
+
+namespace CrystalReportQR
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-L3LK6E7;Initial Catalog=BD_TABLEAU;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Select * from TB_E01_EBITDA_ANUAL",con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            ReportDocument crp = new ReportDocument();
+
+            crp.Load(Server.MapPath("CrystalReport2.rpt"));
+            crp.SetDataSource(ds.Tables["table"]);
+
+            CrystalReportViewer1.ReportSource = crp;
+            crp.ExportToHttpResponse(ExportFormatType.PortableDocFormat,Response, false, "Relatorio Ebitda");
+
+        }
+    }
+}
